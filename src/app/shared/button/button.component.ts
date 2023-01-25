e4ed527cf169ba4188ec0bb18ef98c3ce8bc1b21
NightWalker Yao <huntr@pm.me>
2023-01-25 17:06:31 +0000

    Initial commit

---
+++ src/app/shared/button/button.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-button',
+  templateUrl: './button.component.html',
+  styleUrls: ['./button.component.css']
+})
+export class ButtonComponent {
+  text = 'Click me!'
+
+  clickHandler() {
+    alert('You clicked!')
+  }
+}
